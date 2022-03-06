14a0ef60cc3c7e25e6c9f3e6e24c1989b716b4c1
Escoteirim <Escoteirim@REVISION-PC>
2022-03-06 03:08:10 -0300

    Adicionar arquivos de projeto.

---
+++ CursoWindowsForms/Formulários Curso 2/Frm_Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Login : Form
+    {
+        public string Senha;
+        public string Login;
+        public Frm_Login()
+        {
+            InitializeComponent();
+
+            Lbl_Login.Text = "Usuário";
+            Lbl_PassWord.Text = "Senha";
+            Btn_Ok.Text = "Ok";
+            Btn_Cancel.Text = "Cancel";
+        }
+
+        private void Btn_Ok_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+
+            Senha = Txt_PassWord.Text;
+            Login = Txt_Login.Text;
+            this.Close();
+        }
+
+        private void Btn_Cancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
