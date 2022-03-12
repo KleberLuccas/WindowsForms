06b8db7d926b831faa0aee1afdcf1e9adf8e3657
KLEBER SANTANA <kleberluccas062@gmail.com>
2022-03-12 00:15:05 -0300

    update

--- CursoWindowsForms/Frm_Menu_Flutuante.cs
+++ CursoWindowsForms/Frm_Menu_Flutuante.cs
@@ -21,8 +21,56 @@ namespace CursoWindowsForms
         {
             if (e.Button == MouseButtons.Right)
             {
-                MessageBox.Show("Direita");
+                //var posicaoX = e.X;
+                //var posicaoY = e.Y;
+
+                //MessageBox.Show("O botão clicado foi o da direita. A posição relativa é (" + posicaoX.ToString() + "," + posicaoY.ToString() + ")");
+                //Instanciando menu 
+                var ContextMenu = new ContextMenuStrip();
+
+                var vToolTip001 = DesenhaItemMenu("Item Menu 1", "key");
+                var vToolTip002 = DesenhaItemMenu("Item Menu 2", "Frm_ValidaSenha");
+
+                ContextMenu.Items.Add(vToolTip001);
+                ContextMenu.Items.Add(vToolTip002);
+
+                //Adicionando o item do menu ao menu, chamando a função e atribuindo texto
+                //ContextMenu.Items.Add(DesenhaItemMenu("Item Menu 1", "key"));
+                //ContextMenu.Items.Add(DesenhaItemMenu("Item Menu 2", "Frm_ValidaSenha"));
+                //Exibindo o menu na tela.
+                //Parâmetros: Próprio formulario, posição para exibir menu
+                ContextMenu.Show(this, new Point(e.X, e.Y));
+
+                //Instanciando os métodos de clique à função de click.
+                vToolTip001.Click += new EventHandler(vToolTip001_Click);
+                vToolTip002.Click += new EventHandler(vToolTip002_Click);
             }
-        }
+
+            ToolStripMenuItem DesenhaItemMenu(string text, string nomeImagem)
+            {
+                //Instanciando item do menu
+                var vToolTip = new ToolStripMenuItem();
+                //Atribuindo um texto ao item do menu
+                vToolTip.Text = text;
+                //Instanciando variavel do tipo 'Imagem' com o nome da imagem.
+                Image MyImage = (Image)global::CursoWindowsForms.Properties.Resources.ResourceManager.GetObject(nomeImagem);
+                vToolTip.Image = MyImage;
+
+                return vToolTip;
+            }
+
+            void vToolTip001_Click(object sender1, EventArgs e1)
+            {
+                MessageBox.Show("Selecionei a opção 001 do menu");
+            }
+
+            void vToolTip002_Click(object sender2, EventArgs e2)
+            {
+                MessageBox.Show("Selecionei a opção 002 do menu");
+
+            }
+        } 
+
+
     }
 }
